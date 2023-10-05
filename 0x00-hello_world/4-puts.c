59ea7dafa41e00a9756ed890f743c918eb47a7ed
Darth Jade-i <kkfsolutions.ng@gmail.com>
2023-10-06 00:51:48 +0100

    Created 4-puts.c anHello, puts - as the object file.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Simple program that displays a text.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+
+	return (0);
+
+}
