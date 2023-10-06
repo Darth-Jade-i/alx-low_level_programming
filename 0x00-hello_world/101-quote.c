3bb043f936e18b246de4f43eb08616c4a1f8d989
Darth Jade-i <kkfsolutions.ng@gmail.com>
2023-10-06 02:16:20 +0100

    Modified 101-quote.c again.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,18 +2,21 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Description: A simple program that prints text
+ * main - Entry point of the program
+ *
  * Return: Always 1 (Exit code)
  */
 
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	for (int i = 0; message[i] != '\0'; i++)
-	{
-		putchar(message[i]);
-	}
-	return (1);
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    
+    int i = 0;
+    
+    while (message[i] != '\0')
+    {
+	    write(2, &message[i], 1);
+	    i++;
+    }
+    return (1);
 }
